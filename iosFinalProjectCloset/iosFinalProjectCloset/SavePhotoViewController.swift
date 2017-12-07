79343e25f9fe9a851919259947bd76b36c8afb2c
Mandy <MNJFZ9@mail.missouri.edu>
2017-12-07 12:49:15 -0600

    Take photo camera work

--- iosFinalProjectCloset/iosFinalProjectCloset/SavePhotoViewController.swift
+++ iosFinalProjectCloset/iosFinalProjectCloset/SavePhotoViewController.swift
@@ -25,6 +25,7 @@ class SavePhotoViewController: UIViewController, UIImagePickerControllerDelegate
     var discoverySession: AVCaptureDevice.DiscoverySession?
     var photo: UIImage?
     var photoSampleBuffer:CMSampleBuffer?
+    //let videoDeviceInput = defaultDevice()
     
     let sessionQueue = DispatchQueue(label: "session queue",
                                      attributes: [],
@@ -45,11 +46,11 @@ class SavePhotoViewController: UIViewController, UIImagePickerControllerDelegate
     
     
     @IBAction func photoButtonWasPressed(_ sender: Any) {
-
+        let videoDeviceInput = defaultDevice()
         let photoSettings = AVCapturePhotoSettings()
         photoSettings.isHighResolutionPhotoEnabled = true
         //Breaks at this next line
-        if (self.videoDeviceInput?.device.isFlashAvailable)! {
+        if (videoDeviceInput.isFlashAvailable) {
             photoSettings.flashMode = .auto
         } else {
             print("No device available")
@@ -73,6 +74,20 @@ class SavePhotoViewController: UIViewController, UIImagePickerControllerDelegate
             }
         }
         
+    }
+    
+    func defaultDevice() -> AVCaptureDevice {
+        if let device = AVCaptureDevice.default(.builtInDuoCamera,
+                                                for: AVMediaType.video,
+                                                      position: .back) {
+            return device // use dual camera on supported devices
+        } else if let device = AVCaptureDevice.default(.builtInWideAngleCamera,
+                                                       for: AVMediaType.video,
+                                                             position: .back) {
+            return device // use default back facing camera otherwise
+        } else {
+            fatalError("All supported devices are expected to have at least one of the queried capture devices.")
+        }
     }
     
     //use image picker to select an image
